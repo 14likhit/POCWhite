dbf01820d9752b360392f524beee397e288eead6
14likhit <14likhit@gmail.com>
2020-10-27 00:11:19 +0530

    Added test activity in BurgerQueen and McMickeys.

--- app/src/main/java/com/example/pocwhite/MainActivity.kt
+++ app/src/main/java/com/example/pocwhite/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.pocwhite
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pocwhite.child.TestActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.buttonTest);
+        button.setOnClickListener {
+            startActivity(Intent(this, TestActivity::class.java))
+        }
     }
 }
